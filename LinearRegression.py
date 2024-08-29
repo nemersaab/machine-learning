79157d3e01a75e166a66239b6779ee98d5f821f4
nemer saab <nemersaab1945@gmail.com>
2024-08-29 19:20:08 +0300

    cost_function added

--- LinearRegression.py
+++ LinearRegression.py
@@ -16,4 +16,12 @@ def pridictions(x,w,b):
     for i in range(m):
         f_wb[i]=w*x[i] + b
     return f_wb
-
+def cost_function(x,y,w,b):
+    m=x.shape[0] 
+    cost_sum = 0
+    for i in range(m):
+        f_wb = w*x[i]+b
+        cost = (f_wb -y[i])**2
+        cost_sum += cost
+    total_cost = (1/(2*m))*cost_sum
+    return total_cost
